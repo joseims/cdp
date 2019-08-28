9bf4a70c468ea8c6404b59e410c76bdbadca6f79
JOSE IGNACIO MORSCH SCHMID 116110690 <joseims@lcc3-071.lcc.ufcg.edu.br>
2019-08-28 11:51:33 -0300

    show de top

--- linear-regression/linear-regression.py
+++ linear-regression/linear-regression.py
@@ -1,4 +1,5 @@
 from numpy import *
+import matplotlib.pyplot as plt
 
 def compute_error_for_given_point(b, m, points):
         totalError = 0
@@ -17,8 +18,8 @@ def step_grandient(b_current, m_current, points, learning_rate):
     for i in range(len(points)):
         x = points[i,0]
         y = points[i,1]
-        b_gradient += (2/N) * (((m_current * x) + b_current) - y)
-        m_gradient += (2/N) * x * (((m_current * x) + b_current) - y)
+        b_gradient += - (2/N) * (y - ((m_current * x) + b_current))
+        m_gradient += - (2/N) * x * (y - ((m_current * x) + b_current))
     new_b = b_current - (learning_rate * b_gradient)
     new_m = m_current - (learning_rate * m_gradient)
     return [new_b, new_m]
@@ -27,23 +28,51 @@ def step_grandient(b_current, m_current, points, learning_rate):
 def gradient_descent_runner(points, initial_b, initial_m, n_interations, learning_rate):
     b = initial_b
     m = initial_m
-
-    for i in range(n_interations):
-        b,m = step_grandient(b,m,array(points),learning_rate)
-    return [b,m]
+    b_ = initial_b[-1]
+    m_ = initial_m[-1]
+    c = [compute_error_for_given_point(b_,m_,points)]
+    error0 = float('inf')
+    error1 = 0
+    while abs(error1 - error0) > 0.000001:
+        b_,m_ = step_grandient(b_,m_,array(points),learning_rate)
+        b.append(b_)
+        m.append(m_)
+        c_ = compute_error_for_given_point(b_,m_,points)
+        error1 = error0
+        error0 = c_
+        print(c_)
+        c.append(c_)
+    return [b,m,c]
 
 def run():
-    points = genfromtxt('data.csv',delimiter=',')
+    points = genfromtxt('income.csv',delimiter=',')
     #hiperparemeter
-    learning_rate = 0.0001
+    learning_rate = 0.001
     #y = mx + b
-    initial_b = 0
-    initial_m = 0
-    n_interations = 1000
-    [b,m] = gradient_descent_runner(points, initial_b, initial_m, n_interations, learning_rate)
-    print(b)
-    print(m)
+    initial_b = [0]
+    initial_m = [0]
+    n_interations = 50000
+    b,m,c = gradient_descent_runner(points, initial_b, initial_m, n_interations, learning_rate)
+    print(b[-1])
+    print(m[-1])
+    print(compute_error_for_given_point(b[-1],m[-1],points))
+    plt.plot(c,range(len(b)))
+    plt.xlabel("iterations")
+    plt.ylabel("error")
+    plt.show()
+
+    function = points[:,0]*m[-1] + b[-1]
+    plt.plot(points[:,0],function,color='black')
+    plt.scatter(points[:,0],points[:,1],color='red')
+    plt.show()
+
 
 
 if __name__ == '__main__':
     run()
+
+#3. O RSS dimiuiu. Porque a cada interacao ele diminui do valor do theta
+
+#4. learning_rate = 0.001   n_interations = 50000
+
+#6. 0.000001
